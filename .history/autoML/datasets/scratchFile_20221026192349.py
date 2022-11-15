04a7d965c22543c38b2d6b47e4d28931e28ec1ad
csanicola74 <caroline.sanicola@stonybrook.edu>
2022-11-15 12:12:42 -0500

    me

--- .history/autoML/datasets/scratchFile_20221026192349.py
+++ .history/autoML/datasets/scratchFile_20221026192349.py
@@ -4,15 +4,13 @@ sparcs = pd.read_csv('autoML/datasets/data_sparcs.csv')
 
 # create new var called sparcs_los that if length of stay is greater than 5 days, then long, else short
 # convert sparcs['Length of Stay'] to numeric
-sparcs['Length of Stay'] = pd.to_numeric(
-    sparcs['Length of Stay'], errors='coerce')
-sparcs['sparcs_los'] = sparcs['Length of Stay'].apply(
-    lambda x: 'long' if x > 3 else 'short')
+sparcs['Length of Stay'] = pd.to_numeric(sparcs['Length of Stay'], errors='coerce')
+sparcs['sparcs_los'] = sparcs['Length of Stay'].apply(lambda x: 'long' if x > 3 else 'short')
 sparcs['sparcs_los'].value_counts()
 sparcs.drop('Length of Stay', axis=1, inplace=True)
 sparcs.columns
 
-# just get values from sparcs_los
+# jsut get values from sparcs_los
 sparcs_los = sparcs['sparcs_los'].values
 
 # convert array to list
